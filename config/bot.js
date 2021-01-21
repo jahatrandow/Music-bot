5a51b11106432be6c074c92bd59000f20d1d6b57
jahatrandow <77714201+jahatrandow@users.noreply.github.com>
2021-01-20 20:41:57 -0500

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'NzE5OTY5ODI0NDkwNjUxNzk5.Xt_KJg.8rrxpBGv8fg3dhrrQ_2RvTaZLFU',
+        prefix: 'k!',
+        activity: 'LISTENING',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
